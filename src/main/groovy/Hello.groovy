77934e9fab9dfe03986e359f7fcb5f88bf2c4561
Xieyuxing <185246042@qq.com>
2018-09-19 22:51:09 +0800

    哦哦哦哦哦哦

--- src/main/groovy/Hello.groovy
+++ src/main/groovy/Hello.groovy
@@ -3,3 +3,13 @@ println 'Hello Groovy!'
 for(i in 0..2) println 'xxx'
 
 0.upto(2) {println "$it"}
+
+3.times() {println "$it"}
+
+0.step(10,2){println "$it"}
+
+println "cmd cd ../".execute().getClass().name
+
+String obj;
+print obj?.charAt(0)
+print obj?.length
